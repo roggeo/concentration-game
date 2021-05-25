b2baca17a803819d1b1fe340454603d701c29d3b
Geovani Rogerio <geovanirog@gmail.com>
2021-05-24 21:12:05 -0300

    Lista all cards

--- src/services/cards.js
+++ src/services/cards.js
@@ -1,60 +1,19 @@
 export function cardsOptions() {
     return [
-        [
-            {
-                id: 1,
-                done: false,
-                selected: false,
-                image: 'http://jumpingfishes.com/concentration/images/an1.gif',
-            },
-            {
-                id: 2,
-                done: false,
-                selected: false,
-                image: 'http://jumpingfishes.com/concentration/images/an2.gif',
-            }
-        ],
-        [
-            {
-                id: 3,
-                done: false,
-                selected: false,
-                image: 'http://jumpingfishes.com/concentration/images/an3.gif',
-            },
-            {
-                id: 4,
-                done: false,
-                selected: false,
-                image: 'http://jumpingfishes.com/concentration/images/an4.gif',
-            }
-        ],
-        [
-            {
-                id: 5,
-                done: false,
-                selected: false,
-                image: 'http://jumpingfishes.com/concentration/images/an1.gif',
-            },
-            {
-                id: 6,
-                done: false,
-                selected: false,
-                image: 'http://jumpingfishes.com/concentration/images/an2.gif',
-            }
-        ],
-        [
-            {
-                id: 7,
-                done: false,
-                selected: false,
-                image: 'http://jumpingfishes.com/concentration/images/an3.gif',
-            },
-            {
-                id: 8,
-                done: false,
-                selected: false,
-                image: 'http://jumpingfishes.com/concentration/images/an4.gif',
-            }
-        ]
+        'http://jumpingfishes.com/concentration/images/an0.gif',
+        'http://jumpingfishes.com/concentration/images/an1.gif',
+        'http://jumpingfishes.com/concentration/images/an2.gif',
+        'http://jumpingfishes.com/concentration/images/an3.gif',
+        'http://jumpingfishes.com/concentration/images/an4.gif',
+        'http://jumpingfishes.com/concentration/images/an5.gif',
+        'http://jumpingfishes.com/concentration/images/an6.gif',
+        'http://jumpingfishes.com/concentration/images/an7.gif',
+        'http://jumpingfishes.com/concentration/images/an8.gif',
+        'http://jumpingfishes.com/concentration/images/an9.gif',
+        'http://jumpingfishes.com/concentration/images/an10.gif',
+        'http://jumpingfishes.com/concentration/images/an11.gif',
+        'http://jumpingfishes.com/concentration/images/an12.gif',
+        'http://jumpingfishes.com/concentration/images/an13.gif',
+        'http://jumpingfishes.com/concentration/images/an14.gif'
     ];
 }
